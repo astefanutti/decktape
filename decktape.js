02f2789c97aa213290b0a7149df43ae443bd36f3
Antonin Stefanutti <antonin@stefanutti.fr>
2015-05-25 18:41:26 +0200

    Progress bar initial draft implementation

--- decktape.js
+++ decktape.js
@@ -1,6 +1,7 @@
 var page = require("webpage").create(),
     printer = require("printer").create(),
-    totalSlides;
+    system = require('system'),
+    currentSlide, totalSlides;
 
 page.viewportSize = {width: 1280, height: 720};
 //printer.paperSize = {width: "1280px", height: "720px", margin: "0px"};
@@ -10,23 +11,50 @@ page.open("file:///Users/astefanutti/Development/phantomjs/slides.html", functio
         console.log("Unable to load the address!");
         phantom.exit(1);
     } else {
+        currentSlide = 1;
         totalSlides = slideCount();
         printSlide();
     }
 });
 
-var printSlide = function() {
+function printSlide() {
     window.setTimeout(function() {
-        console.log("Printing slide " + currentSlideIndex() + "/" + totalSlides + "...");
+        system.stdout.write('\r' + progressBar());
         printer.printPage(page);
         if (!isLastSlide()) {
             nextSlide();
+            currentSlide++;
             printSlide();
         } else {
+            system.stdout.write("\nPrinted " + totalSlides + " slides\n");
             phantom.exit();
         }
     }, 1000);
-};
+    // TODO: add a function per backend to wait until a particular condition instead of a timeout
+}
+
+// TODO: add progress bar, duration, ETA and file size
+function progressBar() {
+    var cols = [];
+    cols.push("Printing slide # ");
+    cols.push(leftPadding(currentSlideIndex(), totalSlides.toString().length + 2, ' '));
+    cols.push(" (");
+    cols.push(leftPadding(currentSlide, totalSlides.toString().length, ' '));
+    cols.push('/');
+    cols.push(totalSlides);
+    cols.push(")...");
+    return cols.join('');
+}
+
+function leftPadding(str, len, char) {
+    if (typeof str === "number")
+        str = str.toString();
+    var l = len - str.length;
+    var p = [];
+    while (l-- > 0)
+        p.push(char);
+    return p.join('').concat(str);
+}
 
 var slideCount = function() {
     return page.evaluate(function() {
