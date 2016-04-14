328e4438189c2f9a12dcc548a4a9b8557bdb59b5
Antonin Stefanutti <antonin@stefanutti.fr>
2016-04-14 11:53:08 +0200

    Add Shower 2.x plugin

---
+++ plugins/shower-2.x.js
@@ -0,0 +1,64 @@
+function Shower(page) {
+    this.page = page;
+}
+
+Shower.prototype = {
+
+    getName: function () {
+        return 'Shower 2.x';
+    },
+
+    isActive: function () {
+        return this.page.evaluate(function () {
+            return typeof shower === 'object' && typeof shower.modules === 'object';
+        });
+    },
+
+    configure: function () {
+        var resolved;
+        var promise = new Promise(function (fulfill) {
+            resolved = fulfill;
+        });
+        this.page.onCallback = function () {
+            resolved();
+        };
+        this.page.evaluate(function () {
+            shower.modules.require(['shower.global'], function (sh) {
+                window.decktape = {};
+                decktape.shower = sh.getInited()[0];
+                decktape.shower.container.enterSlideMode();
+                window.callPhantom();
+            });
+        });
+        return promise;
+    },
+
+    slideCount: function () {
+        // FIXME: this does not take fragments into account which ideally should be deactivated
+        return this.page.evaluate(function () {
+            return decktape.shower.getSlidesCount();
+        });
+    },
+
+    hasNextSlide: function () {
+        return this.page.evaluate(function () {
+            return decktape.shower.player.getCurrentSlideIndex() + 1 < decktape.shower.getSlidesCount();
+        });
+    },
+
+    nextSlide: function () {
+        this.page.evaluate(function () {
+            decktape.shower.player.next();
+        });
+    },
+
+    currentSlideIndex: function () {
+        return this.page.evaluate(function () {
+            return decktape.shower.player.getCurrentSlideIndex() + 1;
+        });
+    }
+};
+
+exports.create = function (page) {
+    return new Shower(page);
+};
