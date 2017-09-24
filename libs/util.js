111117f91d5476b85d8ac8e7402e257e86a0bbb4
Antonin Stefanutti <antonin@stefanutti.fr>
2017-09-24 21:29:18 +0200

    Clean-up

--- libs/util.js
+++ libs/util.js
@@ -1,7 +1,5 @@
 'use strict';
 
-module.exports.call = f => async val => { await f(val); return val };
-
 module.exports.delay = delay => value => new Promise(resolve => setTimeout(resolve, delay, value));
 
 module.exports.pause = delay => new Promise(resolve => setTimeout(resolve, delay));  
