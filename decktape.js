d59ad7fb67e6c9bce6a031cc2adaeef68640bd4e
Antonin Stefanutti <antonin@stefanutti.fr>
2018-01-03 11:20:59 +0100

    Revert Puppeteer to version 0.12.0 to be compatible with latest Alpine (musl) Chromium

--- decktape.js
+++ decktape.js
@@ -104,7 +104,7 @@ function parseSize(size) {
   const match = size.match(/^(\d+)x(\d+)$/);
   if (match) {
     const [, width, height] = match;
-    return { width: parseInt(width, 10), height: parseInt(height, 10)};
+    return { width: parseInt(width, 10), height: parseInt(height, 10) };
   } else {
     return '<size> must follow the <width>x<height> notation, e.g., \'1280x720\'';
   }
@@ -202,7 +202,7 @@ process.on('unhandledRejection', error => {
 
   console.log('Loading page', options.url, '...');
   const load = page.waitForNavigation({ waitUntil: 'load', timeout: 20000 });
-  page.goto(options.url, { waitUntil: 'networkidle0', timeout: 60000 })
+  page.goto(options.url, { waitUntil: 'networkidle', timeout: 60000 })
     // wait until the load event is dispatched
     .then(response => load.catch(error => response.status !== 200 ? Promise.reject(error) : response))
     .then(response => console.log('Loading page finished with status:', response.status))
