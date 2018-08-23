b45269d4136e30c7296a359dba3e487790a216bf
Antonin Stefanutti <antonin@stefanutti.fr>
2018-08-23 12:36:09 +0200

    Polish

--- libs/util.js
+++ libs/util.js
@@ -2,7 +2,7 @@
 
 module.exports.delay = delay => value => new Promise(resolve => setTimeout(resolve, delay, value));
 
-module.exports.pause = delay => new Promise(resolve => setTimeout(resolve, delay));  
+module.exports.pause = ms => module.exports.delay(ms)();
 
 module.exports.wait = ms => () => module.exports.delay(ms);
 
